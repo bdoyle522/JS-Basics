e92982486296beb8c85b98aed77bdc74c1d0d7cc
Brendan Doyle <bdoyle522@gmail.com>
2015-10-23 14:38:09 -0500

    completed a couple, safety commit

--- closures.js
+++ closures.js
@@ -9,12 +9,11 @@ var outer = function(){
 //Above you're given a function that returns another function which has a closure over the name variable.
 //Invoke outer saving the return value into another variable called 'inner'.
 
-  //Code Here
+var inner = outer();
 
 //Once you do that, invoke inner.
 
-  //Code Here
-
+console.log(inner());
 
 
 //Next problem
@@ -32,8 +31,8 @@ var callFriend = function(){
 //Above you're given a callFriend function that returns another function.
 //Do what you need to do in order to call your function and get 'Calling Jake at 435-215-9248' in your console.
 
-  //Code Here
-
+var calling = callFriend();
+console.log(calling('453-215-9248'));
 
 
 //Next Problem
